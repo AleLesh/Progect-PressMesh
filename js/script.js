d2a4a687b5f9176f3942fee051ae4d67490184c8
AleLesh <leshukoff@gmail.com>
2023-03-18 09:58:53 +0300

    add new pages and style

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.plus img').onclick = function(){
+    document.querySelector('.open').style.display = 'block';
+    document.querySelector('.plus img').style.transform = rotate(45deg); 
+}
+
+document.querySelector('.plus img').onclick = function(){
+    document.querySelector('.close').style.display = 'none';
+}
